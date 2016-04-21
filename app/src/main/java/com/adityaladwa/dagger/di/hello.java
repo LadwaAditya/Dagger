c4e9e70746332fcc7b0f82ac2a86cc7ed7c43576
Aditya <ladwa.aditya@gmail.com>
2016-04-21 12:49:10 +0530

    First Draft

---
+++ app/src/main/java/com/adityaladwa/dagger/di/hello.java
@@ -0,0 +1,7 @@
+package com.adityaladwa.dagger.di;
+
+/**
+ * Created by Aditya on 21-Apr-16.
+ */
+public class hello {
+}
---
+++ app/src/main/java/com/adityaladwa/dagger/di/module/AppModule.java
@@ -0,0 +1,26 @@
+package com.adityaladwa.dagger.di.module;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Aditya on 21-Apr-16.
+ */
+@Module
+public class AppModule {
+    Application mApplication;
+
+    public AppModule(Application mApplication) {
+        this.mApplication = mApplication;
+    }
+
+    @Provides
+    @Singleton
+    Application provideApplication() {
+        return mApplication;
+    }
+}
